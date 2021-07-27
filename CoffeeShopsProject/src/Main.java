dcfa80503fd41552d62b054b6c17cc3a95f8a0de
songultokman <songultkmn@gmail.com>
2021-07-28 02:35:41 +0300

    Update Main.java

--- CoffeeShopsProject/src/Main.java
+++ CoffeeShopsProject/src/Main.java
@@ -2,22 +2,18 @@
 import java.rmi.RemoteException;
 
 import Adapters.MernisServiceAdapter;
-//import Adapters.MernisServiceAdapter;
 import Concrete.BaseCustomerManager;
 import Concrete.NeroCustomerManager;
 import Concrete.StarbucksCustomerManager;
 import Entities.Customer;
 
 public class Main {
-
-
+	
 	public static void main(String[] args) throws NumberFormatException, RemoteException {
 		
 		BaseCustomerManager customerManager=new StarbucksCustomerManager(new MernisServiceAdapter());
-		customerManager.save(new Customer(1,"12345678901","Songül","Tokman",1987));
+		customerManager.save(new Customer(1,"12345678901","SongÃ¼l","Tokman",1987));
        
-		
-
 	}
 
 }
